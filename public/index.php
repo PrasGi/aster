3d156b50052e7350f93847f0b2cd7dfa6b461a71
Prasetyo Putra pratama <prastyopratama01@gmail.com>
2023-02-12 11:27:34 +0700

    1.7

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+redirect()->to('aster-4');
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
